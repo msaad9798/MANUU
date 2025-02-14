cc0faf84353a1d225a4d36f1144cc8818c74ae53
msaad9798 <saadayinas9322@gmail.com>
2025-02-14 16:39:11 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the term: ");
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int sum = 0;
+        for(int i=1; i<=n; i++) {
+            System.out.print(a+" ");
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+    }
+}
